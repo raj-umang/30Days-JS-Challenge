c22c855dcb171e3e5b0ab26ad37f37f30e823eea
raj-umang <jackerumang@gmail.com>
2024-07-13 16:23:33 +0530

    done - Day 01

---
+++ Day 01/index.js
@@ -0,0 +1,38 @@
+// Task 01
+var first = 10
+console.log(first)
+
+// Task 02
+let sec = 'umang'
+console.log(sec)
+
+// Task 03
+const three = true
+console.log(three)
+
+// Task 04
+console.log(typeof(first))
+console.log(typeof(sec))
+console.log(typeof(three))
+
+let obj = []
+console.log(typeof(obj))
+
+let arr = {
+    name: 'umang',
+    age: 21,
+}
+console.log(typeof(arr))
+
+// Task 05
+let five = 78
+console.log(five)
+five = 89
+console.log(five)
+
+//Task 06
+const six = 23
+console.log(six)
+
+six = 56
+console.log(six)
